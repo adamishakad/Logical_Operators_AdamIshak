bfc717f89ac9f3403471fdf1b2e13ffc55e98328
adamishakad <155333634+adamishakad@users.noreply.github.com>
2024-02-04 09:43:05 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Logical_Operators_AdamIshak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
